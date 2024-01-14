37fe2f58f35d3281cd6a59d8cb5bb2e88ca2cec1
safwasalem44 <sahmed@mmemdev.com>
2024-01-14 18:28:35 +0200

    home

--- App/AppIndex.tsx
+++ App/AppIndex.tsx
@@ -1,4 +1,4 @@
-import React,{useEffect} from 'react';
+import React, {useEffect} from 'react';
 import {StatusBar, useColorScheme} from 'react-native';
 import {createStackNavigator} from '@react-navigation/stack';
 import {
@@ -7,35 +7,36 @@ import {
 } from '@react-navigation/native';
 import Home from './screens/Home';
 import {MyDarkTheme, MyLightTheme} from './utils/themes';
-import { useAppDispatch, useAppSelector } from './utils/hooks';
-import { changeTheme } from './store/themeSlice';
+import {useAppDispatch, useAppSelector} from './utils/hooks';
+import {changeTheme} from './store/themeSlice';
 
 const AppIndex: React.FC<NavigationContainerProps> = () => {
-  const dispatch = useAppDispatch()
+  const dispatch = useAppDispatch();
   const scheme = useColorScheme();
   const Stack = createStackNavigator();
-  const colorThemeDevice = useAppSelector(state => state.theme.theme)
+  const colorThemeDevice = useAppSelector(state => state.theme.theme);
 
   useEffect(() => {
-    dispatch(changeTheme(scheme))
+    dispatch(changeTheme(scheme));
   }, [scheme]);
 
   return (
-      <NavigationContainer
-        theme={colorThemeDevice=== 'dark' ? MyDarkTheme : MyLightTheme}
-        children={undefined}>
-        <StatusBar
-          barStyle={colorThemeDevice=== 'dark' ? 'light-content' : 'dark-content'}
-          backgroundColor={colorThemeDevice=== 'dark' ? '#000' : 'white'}
-        />
-        <Stack.Navigator
-          initialRouteName={'Home'}
-          screenOptions={{
-            headerShown: false,
-          }}>
-          <Stack.Screen name="Home" component={Home} />
-        </Stack.Navigator>
-      </NavigationContainer>
+    <NavigationContainer
+      theme={colorThemeDevice === 'dark' ? MyDarkTheme : MyLightTheme}>
+      <StatusBar
+        barStyle={
+          colorThemeDevice === 'dark' ? 'light-content' : 'dark-content'
+        }
+        backgroundColor={colorThemeDevice === 'dark' ? '#000' : 'white'}
+      />
+      <Stack.Navigator
+        initialRouteName={'Home'}
+        screenOptions={{
+          headerShown: false,
+        }}>
+        <Stack.Screen name="Home" component={Home} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 
